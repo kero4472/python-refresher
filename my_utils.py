2003c6daef25b6f78a353261f3005c63752bb790
kero4472 <kero4472@colorado.edu>
2026-08-28 14:20:35 -0600

    fixed get_column
    get_column now parses through data and will return data for given country.

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,9 @@
 def get_column(file_name, query_column, query_value, result_column):
+    with open(file_name, 'r') as file:
+        for line in file:
+            columns = line.strip().split(',')
+            if len(columns) <= max(query_column, result_column):
+                continue
+            if columns[query_column] == query_value:
+                return columns[result_column]
     return None
