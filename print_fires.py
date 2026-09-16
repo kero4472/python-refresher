e7c0f62e16704da48ced64b7d9f3e399722e07fb
kero4472 <kero4472@colorado.edu>
2026-09-16 12:29:27 -0600

    Added in argparse
    Updated print_fires.py to take the command-line arguments using argparse

--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,37 @@
 import my_utils
+import argparse
 
-country='United States of America'
-country_column = 0
-fires_column = 29
-file_name = 'Agrofood_co2_emission.csv'
+parser = argparse.ArgumentParser(
+    description='Prints the number of fires in a given country from a CSV file.',
+    prog='print_fires')
+
+parser.add_argument('--country', 
+                    type=str, 
+                    help='The country for which to print fire data.',
+                    required=True)
+
+parser.add_argument('--country_column',
+                    type=int, 
+                    help='The index of the country column in the CSV file.',
+                    required=True)
+
+parser.add_argument('--fires_column',
+                    type=int, 
+                    help='The index of the fires column in the CSV file.',
+                    required=True)
+
+parser.add_argument('--file_name',
+                    type=str,
+                    help='The name of the CSV file to read from.',
+                    required=True)
+
+args = parser.parse_args()
+
+country = args.country
+country_column = args.country_column
+fires_column = args.fires_column
+file_name = args.file_name
+
 fires = my_utils.get_column(file_name, country_column, country, result_column=fires_column)
+
 print(fires)
