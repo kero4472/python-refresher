6f0b8b3f2e144e57d827cba67247cf3616ab5344
kero4472 <kero4472@colorado.edu>
2026-09-17 12:22:38 -0600

    Updated get_column
    Update get_column to return a list of integers and catch possible exceptions when opening files and
    converting values to integers

--- my_utils.py
+++ my_utils.py
@@ -1,10 +1,34 @@
 def get_column(file_name, query_column, query_value, result_column=1):
-    results= []
-    with open(file_name, 'r') as file:
-        for line in file:
-            columns = line.strip().split(',')
-            if len(columns) <= max(query_column, result_column):
-                continue
-            if columns[query_column] == query_value:
-                results.append(columns[result_column])
+
+    results = []
+
+    try:
+        with open(file_name, 'r') as file:
+
+            for line in file:
+
+                columns = line.strip().split(',')
+
+                if len(columns) <= max(query_column, result_column):
+                    continue
+
+                if columns[query_column] == query_value:
+
+                    try:
+                        value = int(columns[result_column])
+                        results.append(value)
+
+                    except ValueError:
+                        print(
+                            'Could not convert '
+                            + columns[result_column]
+                            + ' to an integer'
+                        )
+
+    except FileNotFoundError:
+        print('Could not find ' + file_name)
+
+    except PermissionError:
+        print('Could not open ' + file_name)
+
     return results
